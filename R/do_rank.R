d3120b5365f5e4e60123bce902091695ca15ce6c
bczekanski <Ben@bens-mbp-2.middlebury.edu>
2016-04-30 11:29:43 -0400

    Stuck on cleaning that data

--- R/do_rank.R
+++ R/do_rank.R
@@ -1,11 +1,11 @@
 do_rank <- function(x) {
   y <- x %>%
     #na.omit() %>%
-    group_by(j.ret, k.ret, monthYear) %>%
+    group_by(j.ret,monthYear) %>%
     mutate(prev.ret.rank = ntile(prev.returns, 10)) %>%
-    group_by(j.ret, k.ret, monthYear) %>%
+    group_by(j.ret,monthYear) %>%
     mutate(prev.vol.rank = ntile(prev.vol, 3)) %>%
-    group_by(j.ret, k.ret, monthYear) %>%
+    group_by(k.ret, monthYear) %>%
     mutate(sz.rank = ntile(cap.usd, 10)) %>%
     ungroup
   return(y)
--- R/tbl_one.R
+++ R/tbl_one.R
@@ -1,12 +1,30 @@
 tbl_one <- function(x){
   library(htmlTable)
-  y <- select_data(x, c(3, 6, 9, 12), c(3, 6, 9, 12, 24, 36, 48, 60))
-  z <- y %>%
-    spread(key = k.ret, value = future.returns) %>%
-    na.omit() %>%
-    group_by(j.ret, prev.ret.rank) %>%
-    summarize_each(funs(mean), prev.returns, prev.vol, sz.rank, price, c(15:20)) %>%
+  y <- select_data(x, c(3, 6, 9, 12), c(3, 6, 9, 12, 24, 36, 48, 60)) %>%
     filter(prev.ret.rank == 1| prev.ret.rank == 5| prev.ret.rank == 10) %>%
+    group_by(j.ret, k.ret, prev.ret.rank, monthYear) %>%
+    summarize_each(funs(mean), prev.returns, prev.vol, sz.rank, price, future.returns)
+  z <- y %>%
+    select(j.ret, k.ret, prev.ret.rank, future.returns) %>%
+    group_by(j.ret, k.ret, prev.ret.rank) %>%
+    summarize(future.returns = mean(future.returns)) %>%
+    spread(key = prev.ret.rank, value = future.returns) %>%
+    mutate(`0` = `10` - `1`) %>%
+    #select(j.ret, k.ret, diff) %>%
+    group_by(j.ret, k.ret) %>%
+    gather(key = prev.ret.rank, value = future.returns, `0`, `1`, `5`, `10`) %>%
+    mutate(prev.ret.rank  = as.integer(prev.ret.rank))
+   y <- y %>%
+    group_by(j.ret, k.ret, prev.ret.rank) %>%
+    summarize_each(funs(mean), prev.returns, prev.vol, sz.rank, price, future.returns)
+  w <- left_join(z,y, by = c("j.ret", "k.ret", "prev.ret.rank", "future.returns"))
+  # Spread ret rank out and calc diff then gather it back
 
- return(z)
+  v <- w %>%
+    #unite(c(7:12)) %>%
+    spread(key = k.ret, value = future.returns) %>%
+    #na.omit() %>%
+    #summarize_each(funs(mean), prev.returns, prev.vol, sz.rank, price, c(7:12))
+
+ return(v)
 }
