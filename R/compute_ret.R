5590d332b1a8a79325446408bb05537d723ba9ee
bczekanski <Ben@bens-mbp-2.middlebury.edu>
2016-05-01 11:05:36 -0400

    Data in package

--- R/compute_ret.R
+++ R/compute_ret.R
@@ -5,7 +5,7 @@ compute_ret <- function(y) {
       mutate(returns = roll_mean(monthly_returns, months, fill = NA, align = "right")) %>%
       rename(c(returns = paste0(months, "_month_prior_returns")))
   }
-  for(i in c(1:36)) {prior_returns(y, i) -> y}
+  for(i in c(1, 2, 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36)) {prior_returns(y, i) -> y}
   y <- y%>%
     gather(key = j.ret, value = prev.returns, 7:ncol(y))
   y$j.ret  <- extract_numeric(y$j.ret)
@@ -17,7 +17,7 @@ compute_ret <- function(y) {
       rename(c(mean_TV = paste0(months, "_month_prior_volume")))
   }
 
-  for(i in c(1:36)) {prior_returns2(y, i) -> y}
+  for(i in c(1, 2, 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36)) {prior_returns2(y, i) -> y}
   y <- y%>%
     gather(key = j.vol, value = prev.vol, 9:ncol(y))
   y$j.vol  <- extract_numeric(y$j.vol)
@@ -29,7 +29,7 @@ compute_ret <- function(y) {
       rename(c(returns2 = paste0(months, "_month_future_returns")))
   }
 
-  for(i in c(1:36)) {prior_returns3(y, i) -> y}
+  for(i in c(1, 2, 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36)) {prior_returns3(y, i) -> y}
   y <- y %>%
     gather(key = k.ret, value = future.returns, 11:ncol(y))
   y$k.ret  <- extract_numeric(y$k.ret)
