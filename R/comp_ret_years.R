1ff5042ad8f74ddf9179752272994efe19890546
bczekanski <Ben@Bens-MacBook-Pro-2.local>
2016-05-18 08:33:04 -0400

    Table six mostly complete, outlining Paper

--- R/comp_ret_years.R
+++ R/comp_ret_years.R
@@ -24,6 +24,18 @@ compute_ret_years <- function(y) {
     gather(key = j.vol, value = prev.vol, 9:ncol(y))
   y$j.vol  <- extract_numeric(y$j.vol)
 
+  prior_returns3 <- function(y, months){
+    y %>%
+      group_by(symbol) %>%
+      mutate(returns2 = roll_mean(lead(monthly_returns, n = 1), months, fill = NA, align = "left")) %>%
+      rename(c(returns2 = paste0(months, "_month_future_returns")))
+  }
+
+  for(i in c(1:5)) {prior_returns3(y, i) -> y}
+  y <- y %>%
+    gather(key = k.ret, value = future.returns, 11:ncol(y))
+  y$k.ret  <- extract_numeric(y$k.ret)
+
   y <- y %>%
     filter(j.ret == j.vol)
 
--- R/tbl_six.R
+++ R/tbl_six.R
@@ -1,1 +1,42 @@
-tbl_six <- function(){}
+tbl_six <- function(w) {
+  t <- d %>%
+    #group_by(k.ret, prev.vol.rank, prev.ret.rank) %>%
+    #mutate_each(funs(mean), prev.returns, prev.vol, sz.rank, price) %>%
+    group_by(k.ret, prev.ret.rank, prev.vol.rank) %>%
+    summarize(future.returns = mean(yearly_returns)) %>%
+    #ungroup %>%
+    filter(prev.ret.rank == 1| prev.ret.rank == 5| prev.ret.rank == 10)
+
+  s <- t %>%
+    #group_by(j.ret, k.ret, prev.vol.rank) %>%
+    #summarize(future.returns = mean(future.returns)) %>%
+    spread(key = prev.ret.rank, value = future.returns) %>%
+    mutate(`0` = `10` - `1`) %>%
+    #select(j.ret, k.ret, diff) %>%
+    group_by(k.ret, prev.vol.rank) %>%
+    gather(key = prev.ret.rank, value = future.returns, `0`, `1`, `5`, `10`) %>%
+    mutate(prev.ret.rank  = as.integer(prev.ret.rank))
+  f <- s %>%
+    group_by(k.ret, prev.ret.rank, prev.vol.rank) %>%
+    summarize(future.returns = mean(future.returns)) %>%
+    spread(key = prev.vol.rank, value = future.returns) %>%
+    mutate(`0` = `3` - `1`) %>%
+    group_by(k.ret, prev.ret.rank) %>%
+    gather(key = prev.vol.rank, value = future.returns, `0`, `1`, `2`, `3`) %>%
+    #select(- c(`2`)) %>%
+    spread(key = k.ret, value = future.returns) %>%
+    arrange(prev.vol.rank) %>%
+    tbl_df
+  v1 <- f %>%
+    filter(prev.vol.rank == 1)
+  v2 <- f %>%
+    filter(prev.vol.rank == 2)
+  v3 <- f %>%
+    filter(prev.vol.rank == 3)
+
+  e <- cbind(v1, v2)
+  f <- cbind(e, v3) %>%
+    select (- contains ("j.ret"))
+
+  return(w)
+}
